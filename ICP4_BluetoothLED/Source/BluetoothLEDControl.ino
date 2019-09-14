016c49466b49c19774bd0bcaa6480f187c82e7d2
Daniel <s0720bae@gmail.com>
2019-09-13 21:47:48 -0500

    ICP4
    Added ICP4 and Changed some folder names

---
+++ ICP4_BluetoothLED/Source/BluetoothLEDControl.ino
@@ -0,0 +1,34 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTSerial (0,1);
+int led = 8;
+char val;
+void setup()
+{
+
+pinMode(led, OUTPUT);  // Digital pin 13 set as output Pin
+Serial.begin(9600);
+}
+
+void loop()
+{
+  while (Serial.available() > 0)
+  {
+  val = Serial.read();
+  Serial.println(val);
+  }
+
+  if( val == '1') // Forward
+    {
+     
+      digitalWrite(led, HIGH);
+    }
+  else if(val == '2') // Backward
+    {
+ 
+      digitalWrite(led, LOW);
+    }
+
+   
+}
+
+// End of program
